4cc162fcd2804c9c13601cb74a490ea23c44aabc
notBoonk <103270313+notBoonk@users.noreply.github.com>
2022-05-11 12:15:45 -0400

    Updated

---
+++ Exchange-Old.sol
@@ -0,0 +1,85 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract Exchange is Ownable {
+
+    mapping(address => mapping(uint256 => Listing)) public ListingsByTokenAddress;
+    struct Listing {
+        address tokenAddress;
+        uint256 id;
+        uint256 price;
+        uint256 expiration;
+        address owner;
+    }
+    
+    // Admin Functions
+
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    // External Validation Functions
+
+    function isValidListing (address _tokenAddress, uint256 _tokenId) external view returns(bytes memory) {
+        return abi.encodePacked("this,is,my,listing");
+    }
+    
+    function isValidListing (address _tokenAddress, uint256 _tokenId) external view returns(bool) {
+        Listing memory _Listing = ListingsByTokenAddress[_tokenAddress][_tokenId];
+        return (
+            block.timestamp <= _Listing.expiration &&
+            isOwner(_Listing.tokenAddress, _Listing.id, _Listing.owner)
+        );
+    }
+
+    // Internal Validation Functions
+
+    function isValidPurchaseOrder (Listing memory _listing) internal view returns(bool) {
+        return (
+            msg.value == _listing.price &&
+            block.timestamp <= _listing.expiration &&
+            msg.sender != _listing.owner &&
+            isOwner(_listing.tokenAddress, _listing.id, _listing.owner)
+        );
+    }
+
+    function isOwner (address _tokenAddress, uint256 _tokenId, address _user) internal view returns(bool) {
+        IERC721 TokenAddress = IERC721(_tokenAddress);
+        return TokenAddress.ownerOf(_tokenId) == _user;
+    }
+
+    function isApprovalSet (address _tokenAddress, address _user) internal view returns(bool) {
+        IERC721 TokenAddress = IERC721(_tokenAddress);
+        return TokenAddress.isApprovedForAll(_user, address(this));
+    }
+
+    // Listing Functions
+
+    function processPurchaseOrder (address _tokenAddress, address _owner, address _receiver, uint256 _tokenId) internal {
+        delete ListingsByTokenAddress[_tokenAddress][_tokenId];
+       
+        IERC721 TokenAddress = IERC721(_tokenAddress);
+        TokenAddress.transferFrom(_owner, _receiver, _tokenId);
+        
+        payable(_owner).transfer(msg.value - (msg.value / 100));
+    }
+
+    function setListing (address _tokenAddress, uint256 _tokenId, uint256 _price, uint256 _expiration) external {
+        require(isOwner(_tokenAddress, _tokenId, msg.sender));
+        require(isApprovalSet(_tokenAddress, msg.sender));
+
+        ListingsByTokenAddress[_tokenAddress][_tokenId] = Listing(_tokenAddress, _tokenId, _price, _expiration, msg.sender);
+    }
+
+    function buyListing (address _tokenAddress, uint256 _tokenId) external payable {
+        Listing memory _Listing = ListingsByTokenAddress[_tokenAddress][_tokenId];
+        require(isValidPurchaseOrder(_Listing));
+
+        processPurchaseOrder(_tokenAddress, _Listing.owner, msg.sender, _tokenId);
+    }
+
+}
--- Exchange.sol
+++ Exchange.sol
@@ -7,8 +7,14 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract Exchange is Ownable {
 
-    mapping(address => mapping(uint256 => Listing)) public ListingsByTokenAddress;
-    struct Listing { address tokenAddress; uint256 id; uint256 price; uint256 expiration; address owner; }
+    mapping(uint256 => Listing) public Listings;
+    struct Listing {
+        address seller;
+        address buyer;
+        address tokenAddress;
+        uint256 tokenId;
+        uint256 price;
+    }
     
     // Admin Functions
 
@@ -16,26 +22,7 @@ contract Exchange is Ownable {
         payable(msg.sender).transfer(address(this).balance);
     }
 
-    // External Validation Functions
-    
-    function isValidListing (address _tokenAddress, uint256 _tokenId) external view returns(bool) {
-        Listing memory _Listing = ListingsByTokenAddress[_tokenAddress][_tokenId];
-        return (
-            block.timestamp <= _Listing.expiration &&
-            isOwner(_Listing.tokenAddress, _Listing.id, _Listing.owner)
-        );
-    }
-
     // Internal Validation Functions
-
-    function isValidPurchaseOrder (Listing memory _listing) internal view returns(bool) {
-        return (
-            msg.value == _listing.price &&
-            block.timestamp <= _listing.expiration &&
-            msg.sender != _listing.owner &&
-            isOwner(_listing.tokenAddress, _listing.id, _listing.owner)
-        );
-    }
 
     function isOwner (address _tokenAddress, uint256 _tokenId, address _user) internal view returns(bool) {
         IERC721 TokenAddress = IERC721(_tokenAddress);
@@ -49,27 +36,27 @@ contract Exchange is Ownable {
 
     // Listing Functions
 
-    function processPurchaseOrder (address _tokenAddress, address _owner, address _receiver, uint256 _tokenId) internal {
-        delete ListingsByTokenAddress[_tokenAddress][_tokenId];
-       
-        IERC721 TokenAddress = IERC721(_tokenAddress);
-        TokenAddress.transferFrom(_owner, _receiver, _tokenId);
-        
-        payable(_owner).transfer(msg.value - (msg.value / 100));
+    function generateListingId (Listing memory _listing) internal view returns(uint256) {
+        return uint256(keccak256(abi.encodePacked(abi.encodePacked(_listing.seller, _listing.buyer, _listing.tokenAddress, _listing.tokenId, _listing.price))));
     }
 
-    function setListing (address _tokenAddress, uint256 _tokenId, uint256 _price, uint256 _expiration) external {
+    function createListing (address _buyer, address _tokenAddress, uint256 _tokenId, uint256 _price) external returns(uint256) {
         require(isOwner(_tokenAddress, _tokenId, msg.sender));
         require(isApprovalSet(_tokenAddress, msg.sender));
 
-        ListingsByTokenAddress[_tokenAddress][_tokenId] = Listing(_tokenAddress, _tokenId, _price, _expiration, msg.sender);
-    }
+        Listing memory _listing = Listing(
+            msg.sender,
+            _buyer,
+            _tokenAddress,
+            _tokenId,
+            _price
+        );
 
-    function buyListing (address _tokenAddress, uint256 _tokenId) external payable {
-        Listing memory _Listing = ListingsByTokenAddress[_tokenAddress][_tokenId];
-        require(isValidPurchaseOrder(_Listing));
+        uint256 listingId = generateListingId(_listing);
 
-        processPurchaseOrder(_tokenAddress, _Listing.owner, msg.sender, _tokenId);
+        Listings[listingId] = _listing;
+
+        return listingId;
     }
 
 }
